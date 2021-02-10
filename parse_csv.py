adc81e12a03ee9a675eb5b2fcb0dcc44cddd9c18
canbaoafeizai <fczcmcm@gmail.com>
2021-02-10 20:53:09 +0800

    v1.1.2

--- parse_csv.py
+++ parse_csv.py
@@ -1,10 +1,11 @@
+import numpy
 import pandas as pd
 import json
 import demjson
 import numpy as np
 import login
 import os
-
+import math
 json_path = login.json_path
 
 
@@ -26,16 +27,22 @@ class parse(object):
         json_line = {}
         column_num = data.shape[0]
         raw_num = data.shape[1]
-        try:
-            with open(self.json_file_path, "w") as f:
-                for i in range(column_num):
-                    for j in range(raw_num):
+        with open(self.json_file_path, "w") as f:
+            for i in range(column_num):
+                for j in range(raw_num):
+                    if type(data.loc[i][j]) == int or type(data.loc[i][j]) == numpy.int64:
                         json_line[self.csv_column_list[j]] = data.loc[i][j]
-                    f.write(json.dumps(json_line, cls=NpEncoder)+"\n")
-            print("conver success,name={}".format(self.json_file_path))
-            f.close()
-        except:
-            print("csv to json failed!")
+                    elif type(data.loc[i][j]) == str:
+                        json_line[self.csv_column_list[j]] = data.loc[i][j]
+                    elif math.isnan(data.loc[i][j]):
+                        json_line[self.csv_column_list[j]] = "null"
+                    else:
+                        json_line[self.csv_column_list[j]] = data.loc[i][j]
+                f.write(json.dumps(json_line, cls=NpEncoder)+"\n")
+        print("conver success,name={}".format(self.json_file_path))
+        f.close()
+        # except:
+        #     print("csv to json failed!")
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
